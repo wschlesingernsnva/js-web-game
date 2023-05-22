3bb586dc29f4799ece6694e7b0bfc3afb3a78830
wschlesingernsnva <wschlesinger@newschoolva.org>
2023-05-22 14:36:33 -0400

    ok starting over

--- game.js
+++ game.js
@@ -11,34 +11,8 @@ function resizeCanvas(canvas) {
 	ctx.scale(ratio, ratio);
 }
 
-let mousex = 0;
-let mousey = 0;
-let start = false;
-
-function fadeOut(canvas) {
-	let ctx = canvas.getContext("2d");
-
-	ctx.beginPath();
-	ctx.fillStyle = "rgba(255, 255, 255, 0.05)";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	ctx.globalCompositeOperation = "source-over";
-}
-
-let hue = 0;
-
 function draw(canvas) {
 	let ctx = canvas.getContext("2d");
-
-	ctx.beginPath();
-	ctx.strokeStyle = "hsl(" + hue + ", 100%, 50%)";
-	if (hue < 360) {
-		hue++;
-	} else {
-		hue = 0;
-	}
-	ctx.moveTo(mousex, mousey);
-	ctx.lineTo(Math.random() * canvas.width, Math.random() * canvas.height);
-	ctx.stroke();
 }
 
 function main() {
@@ -49,16 +23,7 @@ function main() {
 		resizeCanvas(canvas);
 	});
 
-	addEventListener("mousemove", (event) => {
-		if (!start) {
-			setInterval(fadeOut, 10, canvas);
-			setInterval(draw, 10, canvas);
-			start = true;
-		}
-
-		mousex = event.clientX;
-		mousey = event.clientY;
-	});
+	setInterval(draw, 10, canvas);
 }
 
 main();
